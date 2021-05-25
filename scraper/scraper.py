3f496bbb9f961cbfe348d368abbd55e7dcdec168
Alexander DeForge <alexanderdeforge@gmail.com>
2021-05-25 14:05:37 -0400

    Componentized code.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,90 +1,117 @@
 import os
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'), options=options)
-
+CHROMEDRIVER_PATH = os.path.abspath('chromedriver')
 URL = "https://coinmarketcap.com/"
-driver.get(URL)
-html = driver.page_source
+TOP_N = 100
 
-soup = BeautifulSoup(html, features="html.parser")
-rows = soup.find('tbody').findChildren('tr')
 
-result = []
+def setup():
+    options = Options()
+    options.add_argument('--headless')
+    result = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
+    return result
 
-for row in rows:
-    columns = row.findChildren('td')
+def get_hypertext(driver):
+    driver.get(URL)
+    result = driver.page_source
+    return result
 
+def get_table_with_data(html):
+    soup = BeautifulSoup(html, features="html.parser")
+    result = soup.find('tbody').findChildren('tr')
+    return result
+
+def get_coin_name(columns):
     column = columns[2].findChildren('p')
-    name = column[0].text
-    symbol = column[1].text
+    result = column[0].text
+    return result
 
+def get_coin_symbol(columns):
+    column = columns[2].findChildren('p')
+    result = column[1].text
+    return result
+
+def get_coin_price(columns):
     column = columns[3].findChildren('a')
     price = column[0].text
+    price = re.sub(r"[$|,]","",price)  # strip the '$' symbol and ',' symbols
+    result = float(price)    # catch cast exception
+    return result
 
+def get_coin_change24h(columns):
     column = columns[4]
     change24h_sign = 1 if "up" in column.find('span').find('span')['class'][0] else -1
     change24h = column.text
+    change24h = re.sub(r"[%]","",change24h)  # strip '%' symbol
+    change24h = float(change24h)    # catch cast exception
+    result = change24h_sign * change24h
+    return result
 
+def get_coin_change7d(columns):
     column = columns[5]
     change7d_sign = 1 if "up" in column.find('span').find('span')['class'][0] else -1
     change7d = column.text
+    change7d = re.sub(r"[%]","",change7d)  # strip '%' symbol
+    change7d = float(change7d)  # catch cast exception
+    result = change7d_sign * change7d
+    return result
 
+def get_coin_market_cap(columns):
     column = columns[6].findChildren('span')
     market_cap = column[-1].text
+    market_cap = re.sub(r"[$|,]","",market_cap)  # strip '$' symbol and ',' symbols
+    result = int(market_cap)
+    return result
 
+def get_coin_volume24h(columns):
     column = columns[7].find('a').find('p')
     volume24h = column.text
+    volume24h = re.sub(r"[$|,]","",volume24h)  # strip '$' symbol and ',' symbols
+    result = int(volume24h)
+    return result
 
+def get_coin_circulating_supply(columns):
     column = columns[8].find('p')
     circulating_supply = column.text
+    circulating_supply = re.sub(r'[A-Z|\s|,]','',circulating_supply)  # strip ',' symbols, whitespace and coin symbol
+    result = int(circulating_supply)
+    return result
 
-    print(name)
-    print(symbol)
-    print(price)
-    print(change24h_sign)
-    print(change24h)
-    print(change7d_sign)
-    print(change7d)
-    print(market_cap)
-    print(volume24h)
-    print(circulating_supply)
-
-    break
+def get_top_n_coin_data(table_rows):
+    result = []
+    for index, row in enumerate(table_rows):
+        if index >= 2:
+            break
+        columns = row.findChildren('td')
+        coin_data = {}
+        coin_data["name"] = get_coin_name(columns)
+        coin_data["symbol"] = get_coin_symbol(columns)
+        coin_data["price"] = get_coin_price(columns)
+        coin_data["change24h"] = get_coin_change24h(columns)
+        coin_data["change7d"] = get_coin_change7d(columns)
+        coin_data["market_cap"] = get_coin_market_cap(columns)
+        coin_data["volume24h"] = get_coin_volume24h(columns)
+        coin_data["circulating_supply"] = get_coin_circulating_supply(columns)
+        result.append(coin_data)
     
-# import json
-# import requests
-# from bs4 import BeautifulSoup
+    if index+1 < TOP_N:
+        # log error
+        pass
+    
+    return result
 
-# URL = "https://coinmarketcap.com/"
-# html = requests.get(URL).text
+def main():
+    driver = setup()
+    html = get_hypertext(driver)
+    table_rows = get_table_with_data(html)
+    result = get_top_n_coin_data(table_rows)
+    print(result)
 
-# parsed_html = BeautifulSoup(html, features="html.parser")
-
-# json_data = parsed_html.find("script", id="__NEXT_DATA__", type="application/json").string  # by inspection from using the 'curl' command on the URL 'https://coinmarketcap.com/'
-# json_data = json.loads(json_data)
-
-# top_cryptocurriencies = json_data["props"]["initialState"]["cryptocurrency"]["listingLatest"]["data"]
-
-# bitcoin_price = top_cryptocurriencies[0]["quotes"][-1]["price"]  # the price of one bitcoin in USD, the pricees of other cryptocurrencies are a multiplier of this value
-
-# for index, crypto in enumerate(top_cryptocurriencies):
-#     if index >= 2:
-#         break
-#     print(str(index+1))
-#     print(crypto)
-#     print(crypto["name"])  # name
-#     print(crypto["symbol"])  # symbol
-#     print(crypto["quotes"][0]["price"] * bitcoin_price)  # Price X
-#     print(crypto["quotes"][0]["percentChange24h"])  # 24h %
-#     print(crypto["quotes"][0]["percentChange7d"])  # 7d %
-#     print(crypto["quotes"][0]["marketCap"] * bitcoin_price)  # Market Cap X
-#     print(crypto["quotes"][0]["volume24h"])  # Volume 24h X
-#     print(crypto["circulatingSupply"])  # Circulating Supply
-#     print()
+if __name__ == "__main__":
+    main()
